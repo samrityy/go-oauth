372ca7fc4770904f6216efbda331e2b8f71a8e39
samrityy <smritymulmi23@gmail.com>
2025-07-31 14:46:09 +0545

    feat: update users table to include password field and remove unused columns

--- migrations/000001_create_users_table.up.sql
+++ migrations/000001_create_users_table.up.sql
@@ -1,13 +1,9 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    provider TEXT NOT NULL,
-    provider_id TEXT NOT NULL UNIQUE,
-    login TEXT,
     name TEXT,
     email TEXT,
     avatar_url TEXT,
-    access_token TEXT,
-    refresh_token TEXT,
+    password TEXT,
     created_at TIMESTAMPTZ DEFAULT NOW(),
     updated_at TIMESTAMPTZ DEFAULT NOW()
 );
