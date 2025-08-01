57037e3ead7670cd31166b3e3c3b727810c68f5f
samrityy <smritymulmi23@gmail.com>
2025-08-01 19:03:44 +0545

    refactor: remove unused fields from User model

--- internal/models/user.go
+++ internal/models/user.go
@@ -4,15 +4,10 @@ package models
 import "time"
 
 type User struct {
-    ID           string   
-    Provider     string    
-    ProviderID   string    
-    Login        string
+    ID           string       
     Name         string
     Email        string
     AvatarURL    string
-    AccessToken  string
-    RefreshToken string
     CreatedAt    time.Time
     UpdatedAt    time.Time
 }
