cd63f97d3db1dc29fbfc4035b2d5da79280f7a6c
samrityy <smritymulmi23@gmail.com>
2025-08-04 14:18:54 +0545

    refactor: rename oauth_accounts table to user_oauth

--- migrations/000002_create_oauth_accounts_table.down.sql
+++ migrations/000002_create_oauth_accounts_table.down.sql
@@ -1,12 +1,1 @@
-CREATE TABLE oauth_accounts (
-    id SERIAL PRIMARY KEY,
-    user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,  -- foreign key
-    provider TEXT NOT NULL,                 
-    provider_id TEXT NOT NULL,              
-    access_token TEXT,
-    refresh_token TEXT,
-    token_expiry TIMESTAMPTZ,
-
-    UNIQUE (provider, provider_id),         
-    UNIQUE (user_id, provider)            
-);
+DROP TABLE IF EXISTS user_oauth;
---
+++ migrations/000002_create_oauth_accounts_table.up.sql
@@ -0,0 +1,12 @@
+CREATE TABLE user_oauth (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,  -- foreign key
+    provider TEXT NOT NULL,                 
+    provider_id TEXT NOT NULL,              
+    access_token TEXT,
+    refresh_token TEXT,
+    token_expiry TIMESTAMPTZ,
+
+    UNIQUE (provider, provider_id),         
+    UNIQUE (user_id, provider)            
+);
